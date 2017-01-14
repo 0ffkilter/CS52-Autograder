8fcdfe8ca56f6daf7b1dadd6d92e136bb5af6444
Matthew Gee <geem2014@gmail.com>
2017-01-14 01:28:52 -0800

    typos

--- cmd_utils.py
+++ cmd_utils.py
@@ -16,6 +16,7 @@ def progress(count, total, suffix=''):
     Prints a progress bar
 
     Taken from stackoverflow
+
     """
     bar_len = 60
     filled_len = int(round(bar_len * count / float(total)))
@@ -23,7 +24,7 @@ def progress(count, total, suffix=''):
     percents = round(100.0 * count / float(total), 1)
     bar = '=' * filled_len + '-' * (bar_len - filled_len)
 
-    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix.ljust(6)
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix.ljust(6)))
     sys.stdout.flush()  
 
 
